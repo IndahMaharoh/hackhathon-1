e966a3a52fc3bf1ab8279543156b26637e687b85
IndahMaharoh <92769242+IndahMaharoh@users.noreply.github.com>
2025-07-19 15:53:16 +0700

    Update ProdukController.php

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProdukController extends Controller
 {
@@ -11,7 +12,8 @@ class ProdukController extends Controller
      */
     public function index()
     {
-        //
+        $produk=DB::table('produk')->get();
+        return view('produk.produk', compact(['produk']));
     }
 
     /**
